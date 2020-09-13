e446ff9aa86557590e6b1ce239a5f381d1d39849
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-09-13 14:07:42 +0300

    added the class ColorGenerator

---
+++ lib/utils/color_generator.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+import 'dart:ui';
+
+class ColorGenerator {
+  static Random _randomNumbersGenerator = new Random();
+
+  static Color generateRandomColorUsingRGB() {
+    int r = _randomNumbersGenerator.nextInt(255);
+    int g = _randomNumbersGenerator.nextInt(255);
+    int b = _randomNumbersGenerator.nextInt(255);
+
+    return Color.fromARGB(0, r, g, b);
+  }
+}
